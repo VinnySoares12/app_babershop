f13ed26254cc3f54c139377149ba1f9d327d6e89
Vinicius <Viniciusandreguidio@hotmail.com>
2026-05-16 00:59:12 -0300

    Corrige host do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import path from "node:path";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "10.19.34.30",
-    port: 5174,
+    host: true,
+    port: 5173,
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
